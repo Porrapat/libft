34d19946dc033fe2bf9b84f6c66af0d1b756de24
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-02-18 17:15:13 +0700

    Add ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -14,22 +14,29 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*rtn;
+	char	*str;
 	size_t	i;
+	size_t	s_len;
 
-	if (!s)
-		return (NULL);
-	if ((size_t)start > ft_strlen(s))
-		return (ft_strdup(""));
-	rtn = malloc(sizeof(char) * (len + 1));
-	i = 0;
-	if (!rtn)
-		return (0);
-	while (i < len)
+	s_len = ft_strlen(s);
+	if (start >= s_len)
 	{
-		rtn[i] = *(s + start + i);
+		str = (char *) malloc(sizeof(char));
+		str[0] = '\0';
+		return (str);
+	}
+	if (s_len <= len)
+		str = (char *) malloc(sizeof(char) * (s_len - start + 1));
+	else
+		str = (char *) malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len && s[i + start] != '\0')
+	{
+		str[i] = s[i + start];
 		i++;
 	}
-	rtn[i] = '\0';
-	return (rtn);
+	str[i] = '\0';
+	return (str);
 }
