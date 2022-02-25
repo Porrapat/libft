914d2d14e92a6662ded7b6c73c17fa5709a2ed43
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-02-25 20:55:24 +0700

    Return back to better function that pass more strict than Moulinette test case, Thanks Moulinette.

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,8 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	ret;
+	size_t	i;
 
-	ret = ft_memcmp(s1, s2, n);
-	return (ret);
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n - 1 && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
