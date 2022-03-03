d5219946e8e8eac2ba4fb748e04fc6f83f7a9f6e
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2022-03-03 20:19:12 +0700

    Not Good Code Khun Bank ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,25 +14,29 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-	size_t	c;
-	size_t	n_len;
-	char	*hay;
+	size_t	nh;
+	size_t	nn;
+	size_t	nh_temp;
 
-	i = 0;
-	hay = (char *)haystack;
-	n_len = ft_strlen(needle);
-	if (n_len == 0 || haystack == needle)
-		return (hay);
-	while (hay[i] != '\0' && i < len)
+	nh = 0;
+	nn = 0;
+	if (needle[0] == '\0')
+		return ((char *) haystack);
+	while (nh < len && haystack[nh] != '\0')
 	{
-		c = 0;
-		while (hay[i + c] != '\0' && needle[c] != '\0'
-			&& hay[i + c] == needle[c] && i + c < len)
-			c++;
-		if (c == n_len)
-			return (hay + i);
-		i++;
+		if (haystack[nh] == needle[nn])
+		{
+			nh_temp = nh;
+			while (haystack[nh++] == needle[nn++])
+			if (needle[nn] == '\0')
+				return ((char *)haystack + nh_temp);
+			else
+			{
+				nh -= nh_temp;
+				nn = 0;
+			}	
+		}
+		nh++;
 	}
-	return (0);
+	return (NULL);
 }
